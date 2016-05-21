4879f46632e2829ac6cd46f81b1b2908d569e272
sylElena <sylalp7@gmail.com>
2016-05-21 16:46:17 -0600

    --

--- SistAmortizacion/src/Controlador/Controlador.java
+++ SistAmortizacion/src/Controlador/Controlador.java
@@ -10,12 +10,5 @@ package Controlador;
  * @author SilviaElena
  */
 public class Controlador {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
