11896f6eb329ebca224a428152586b5386fbbee3
Kenneth <kmadrian03@gmail.com>
2016-05-21 14:40:43 -0600

    Revert "Amortizaciones"
    This reverts commit b11b9992cdb9619da3d83cc112095a017f22a683.

--- SistAmortizacion/src/sistamortizacion/SistAmortizacion.java
+++ SistAmortizacion/src/sistamortizacion/SistAmortizacion.java
@@ -5,11 +5,6 @@
  */
 package sistamortizacion;
 
-import DataTransferObject.*;
-import Modelo.*;
-import Modelo.Fabrica.FactorySistemaAmortizacion;
-import java.lang.reflect.InvocationTargetException;
-
 /**
  *
  * @author SilviaElena
@@ -19,28 +14,8 @@ public class SistAmortizacion {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    public static void main(String[] args) {
         // TODO code application logic here
-        DTOCliente dtoCliente = new DTOCliente("silvia","elena","alpizar");
-        Cliente cliente = new ClienteFisico(dtoCliente);
-        DTOSistema dtoSistema = new DTOSistema(1000000.0,5,0.15,"colones", cliente);
-        SistemaAmortizacion aleman = new Aleman(dtoSistema);
-        SistemaAmortizacion frances = new Frances(dtoSistema);
-        SistemaAmortizacion americano = new Americano(dtoSistema);
-        
-        System.out.println(aleman.getClass());
-        
-       /* System.out.println(aleman.calcularTablaAmortizacion().toString());
-        System.out.println(frances.calcularTablaAmortizacion().toString());
-        System.out.println(americano.calcularTablaAmortizacion().toString());
-        */
-        FactorySistemaAmortizacion factory = new FactorySistemaAmortizacion();
-        SistemaAmortizacion aleman2 = factory.crearSistemaAmortizacion(dtoSistema);
-        
-        System.out.println(aleman2.getMoneda());
-        
-        
-                
     }
     
 }
