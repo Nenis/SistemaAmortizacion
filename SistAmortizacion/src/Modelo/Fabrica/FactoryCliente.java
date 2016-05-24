67fe7fa092a51008409bf7d1d0b558326b89de7c
sylElena <sylalp7@gmail.com>
2016-05-24 01:22:57 -0600

    Exepciones

--- SistAmortizacion/src/Modelo/Fabrica/FactoryCliente.java
+++ SistAmortizacion/src/Modelo/Fabrica/FactoryCliente.java
@@ -7,38 +7,35 @@ package Modelo.Fabrica;
 
 import DataTransferObject.DTOSistema;
 import Modelo.Cliente;
-import java.lang.reflect.InvocationTargetException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Kenneth
  */
 public class FactoryCliente {
-        
-        private static FactoryCliente instancia = null;
-    
-        private FactoryCliente(){}
-    
 
-        public static FactoryCliente getInstance(){
-            if(instancia == null){
-                FactoryCliente instancia = new FactoryCliente();
-                return instancia;
-            }
-            return FactoryCliente.instancia;  
-        }
-        
-        public Cliente crearCliente(DTOSistema dtoSistema)  {
-            Cliente objeto = null;
-            try {
-                Class c = Class.forName("Modelo.Cliente" + dtoSistema.getTipoCliente());
-                objeto = (Cliente) c.getConstructor(DTOSistema.class).newInstance(dtoSistema);
-            } catch (Exception ex) {
-                System.out.println("No se pudo crear el Cliente");
-            }
-            return objeto;
+    private static FactoryCliente instancia = null;
+
+    private FactoryCliente() {
     }
-    
+
+    public static FactoryCliente getInstance() {
+        if (instancia == null) {
+            instancia = new FactoryCliente();
+            return instancia;
+        }
+        return FactoryCliente.instancia;
+    }
+
+    public Cliente crearCliente(DTOSistema dtoSistema) {
+        Cliente objeto = null;
+        try {
+            Class c = Class.forName("Modelo.Cliente" + dtoSistema.getTipoCliente());
+            objeto = (Cliente) c.getConstructor(DTOSistema.class).newInstance(dtoSistema);
+        } catch (Exception ex) {
+            System.out.println("No se pudo crear el Cliente");
+        }
+        return objeto;
+    }
+
 }
